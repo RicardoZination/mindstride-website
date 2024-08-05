31e3eefed2e003a020e483fa933c08ee3e4842f7
RicardoZination <ricardo.nunez@zination.com>
2024-08-05 19:19:12 -0300

    form

--- userappregisterform/script.js
+++ userappregisterform/script.js
@@ -1,46 +1,54 @@
 document.getElementById('registerForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
+
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    
-    const requestData = {
-        name: name,
+
+    const profile = {
+        first_name: name,
         email: email
     };
-    
-    fetch('https://api.example.com/register', {
+
+    const data = {
+        email,
+        profile,
+    };
+
+    const ENCRYPTION_SECRET_KEY = 'o~HR\xe72\xb4\xa4\xe9\xc4R&-`\xb0om\xca\xa5\x0494\xc7\xed\xce8/"V\xa0\xfcG';
+    const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), ENCRYPTION_SECRET_KEY).toString();
+
+    fetch('http://demo3932735.mockable.io/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(requestData)
+        body: JSON.stringify({ data: encryptedData })
     })
     .then(response => response.json())
     .then(data => {
-        if (data.success) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Registration Successful',
-                text: 'You have been successfully registered!',
-                confirmButtonText: 'OK'
-            });
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Registration Failed',
-                text: 'There was an error with your registration. Please try again.',
-                confirmButtonText: 'OK'
-            });
-        }
+        console.log('Success:', data);
+        Swal.fire({
+            icon: 'success',
+            title: 'Registro exitoso',
+            text: 'Se ha registrado exitosamente.'
+        });
+
+        emailjs.send('service_xyintba', 'template_vhsxwgl', {
+            email: email,
+            name: name
+        })
+        .then(function(response) {
+            console.log('Email sent successfully!', response.status, response.text);
+        }, function(error) {
+            console.error('Failed to send email:', error);
+        });
     })
-    .catch(error => {
+    .catch((error) => {
         console.error('Error:', error);
         Swal.fire({
             icon: 'error',
             title: 'Error',
-            text: 'There was an error with your registration. Please try again.',
-            confirmButtonText: 'OK'
+            text: 'Hubo un problema con el registro. Inténtalo nuevamente.'
         });
     });
 });
